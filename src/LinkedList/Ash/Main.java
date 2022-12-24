6563ff702f3e9210b8e0c08e388a00f19c8b75dd
Ashvin Manwar <manwar.ashvin@gmail.com>
2022-12-24 08:40:46 +0530

    aa

--- src/LinkedList/Ash/Main.java
+++ src/LinkedList/Ash/Main.java
@@ -2,14 +2,14 @@ package LinkedList.Ash;
 
 public class Main {
     public static void main(String[] args) {
-//    LL list=new LL();
-//        list.insertFirst(3);
-//        list.insertFirst(2);
-//        list.insertFirst(8);
-//        list.insertFirst(17);
-//        list.insertLast(99);
+    LL list=new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertLast(99);
 //        list.insert(100, 3);
-//        list.display();
+        list.display();
 //        System.out.println(list.deleteFirst());
 //        list.display();
 //        System.out.println(list.deleteLast());
@@ -19,15 +19,15 @@ public class Main {
 //        list.insertRec(88, 2);
 //        list.display();
 
-        DLL list = new DLL();
-        list.insertFirst(3);
-        list.insertFirst(2);
-        list.insertFirst(8);
-        list.insertFirst(17);
+//        DLL list = new DLL();
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
 //        list.insertLast(99);
 //        list.insert(8, 65);
 //
-        list.display();
+//        list.display();
 
 //        CLL list = new CLL();
 //        list.insert(23);
